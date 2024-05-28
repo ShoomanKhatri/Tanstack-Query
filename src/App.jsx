4cd01e36415bbf2227fde9b6a1ebfdcdf28fa10a
Shooman Khatri <112597601+ShoomanKhatri@users.noreply.github.com>
2024-05-28 10:59:57 +0000

    useMutation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import  './App.css';
 import useProducts from './hooks/useProducts';
+import { useMutation } from '@tanstack/react-query';
 
 import React from 'react';
 
 const App = () => {
+
+  const mutation = useMutation({
+    mutationFn: () => 
+      fetch('https://fakestoreapi.com/products',{
+            method:"POST",
+            body:JSON.stringify(
+                {
+                    title: 'test product',
+                    price: 13.5,
+                    description: 'lorem ipsum set',
+                    image: 'https://i.pravatar.cc',
+                    category: 'electronic'
+                }
+            )
+        })
+            .then(res=>res.json())
+            .then(json=>console.log(json))
+  })
 
   const{isPending,data,error,isError} = useProducts();
 
@@ -16,7 +35,24 @@ const App = () => {
   }
   return (
     <>
-    <button>Add Product</button>
+    {mutation.isPending ? (
+        'Adding product...'
+      ) : (
+        <>
+          {mutation.isError ? (
+            <div>An error occurred: {mutation.error.message}</div>
+          ) : null}
+
+          <button
+            onClick={() => {
+              mutation.mutate()
+            }}
+          >
+            Add Product
+          </button>
+          {mutation.isSuccess ? <div>Product added</div> : null}
+        </>
+      )}
     <div className='products'>
       {
         data.map(product=>{
