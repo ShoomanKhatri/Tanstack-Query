60920a0ebc16804b60ef24c7ed10d876d08b037b
Shooman Khatri <112597601+ShoomanKhatri@users.noreply.github.com>
2024-05-28 08:05:00 +0000

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,36 @@
-import './App.css'
-import useProducts from './hooks/useProducts'
+import  './App.css';
+import useProducts from './hooks/useProducts';
 
-function App() {
+import React from 'react';
 
-  const { isPending, data, error, isError } = useProducts();
+const App = () => {
 
-  if (isPending) {
+  const{isPending,data,error,isError} = useProducts();
+
+  if(isPending){
     return <h1>Loading...</h1>
   }
 
-  if (isError) {
+  if(isError){
     return <h1>{error.message}</h1>
   }
-
   return (
-    <div>
-      {data.map((product) => {
-        return (
-          <div key={product.id}>
-            <img src={product.image} alt={product.title} />
-            <h3>{product.title}</h3>
-            <p>{product.price}</p>
-          </div>
-        )
-      })}
+    <>
+    <button>Add Product</button>
+    <div className='products'>
+      {
+        data.map(product=>{
+         return(
+          <div key={product.id} className='product'>
+          <img src={product.image} alt={product.title} />
+          <h3>{product.title}</h3>
+          <p>{product.price}</p>
+        </div>
+         )
+        })
+      }
     </div>
+    </>
   )
 }
 
--- src/hooks/useProducts.jsx
+++ src/hooks/useProducts.jsx
@@ -4,7 +4,7 @@ import { useQuery } from '@tanstack/react-query'
 const useProducts = () => {
 
     const fetchProducts = async () => {
-        const products = await fetch("https://fakestoreapi.com/products/1");
+        const products = await fetch("https://fakestoreapi.com/products/");
         const data = await products.json();
         return data;
     }
